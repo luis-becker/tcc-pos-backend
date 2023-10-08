0b95efd49c8af20118fc080dd8024a79f47ca640
Luís Becker <luis.s@edu.pucrs.br>
2023-10-08 12:29:26 -0300

    WIP

--- src/v1/utils/mongooseConnector.js
+++ src/v1/utils/mongooseConnector.js
@@ -5,10 +5,19 @@ const database = process.env.MONGO_DATABASE
 const databaseUser = process.env.MONGO_USER
 const databasePassword = process.env.MONGO_PASSWORD
 
-async function connect() {
-  await mongoose.connect(`mongodb://${databaseUser}:${databasePassword}@${host}/${database}`)
-  console.log('Connected to database!')
+class Databse {
+  constructor() {
+    this._connect()
+  }
+  async _connect() {
+    try {
+      await mongoose.connect(`mongodb://${databaseUser}:${databasePassword}@${host}/${database}`)
+      console.log('Database connection successful')
+    } catch (err) {
+      console.error('Database connection error:', err)
+    }
+  }
 }
 
 
-module.exports = {connect}
+module.exports = new Databse
